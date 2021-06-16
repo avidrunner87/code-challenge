e80863ef1a7b5cc4854be5d1007abb614af0e63d
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-06-15 22:37:11 -0500

    Finished the quiz page, scoring, and README page

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # coding-knowledge-challenge
+
+## Description
+At some point as a full-stack web developer, you’ll likely be asked to complete a coding assessment. A typical coding assessment includes both multiple-choice questions and interactive coding challenges. To help familiarize you with these tests with a timed coding quiz with multiple-choice questions.
+
+### User Story
+**AS AN** coding boot camp student<br>
+I **WANT** to take a timed quiz on JavaScript fundamentals that stores high scores<br>
+**SO THAT** I can gauge my progress compared to my peers
+
+### Acceptance Criteria
+**GIVEN** I am taking a code quiz
+
+>WHEN I click the start button<br>
+THEN a timer starts and I am presented with a question
+
+>WHEN I answer a question<br>
+THEN I am presented with another question
+
+>WHEN I answer a question incorrectly<br>
+THEN time is subtracted from the clock
+
+>WHEN all questions are answered or the timer reaches 0<br>
+THEN the game is over
+
+>WHEN the game is over<br>
+THEN I can save my initials and my score
+
+### Mock-up
+The following picture shows the website appearance:
+
+![Website Mock-Up](./assets/images/website-mockup.gif)
+
+## Installation
+To fork this code to further improve the website, please follow these directions:
+
+1. In Github fork the repository.
+1. Clone the repository to your local computer.
+
+_This assumes that you have setup your own SSH keys to connect with Github._
+## Usage
+You can access the website [here](https://avidrunner87.github.io/coding-knowledge-challenge/).
+
+## References
+[MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/HTML/Element)<br>
+[W3 Schools](https://www.w3schools.com/)<br>
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,31 +1,80 @@
 // ------------- Assignment Code -------------
 // Array of questions
 const quizQuestions = {
-    "item": {
-        "question": "What is your name?",
-        "answers": [
-            {
-                "value": "Andrew",
-                "correct": true
-            },
-            {
-                "value": "Jane",
-                "correct": false
-            },
-            {
-                "value": "Sam",
-                "correct": true
-            },
-            {
-                "value": "Terry",
-                "correct": true
-            }
-        ]
-    }
+    "questions": [
+        {
+            "question": "Inside which HTML element do we put the JavaScript",
+            "answers": [
+                {
+                    "value": "<javascript>",
+                    "correct": false
+                },
+                {
+                    "value": "<js>",
+                    "correct": false
+                },
+                {
+                    "value": "<jquery>",
+                    "correct": false
+                },
+                {
+                    "value": "<script>",
+                    "correct": true
+                }
+            ]
+        },
+        {
+            "question": "What is the correct JavaScript syntax to change the content of the following HTML element: <p id=\"demo\">This is a demonstration.</p>",
+            "answers": [
+                {
+                    "value": "#demo.innerHTML = \"Hello World!\";",
+                    "correct": true
+                },
+                {
+                    "value": "document.getElement(\"p\").innerHTML = \"Hello World!\";",
+                    "correct": false
+                },
+                {
+                    "value": "document.getElementByName(\"p\").innerHTML = \"Hello World!\";",
+                    "correct": false
+                },
+                {
+                    "value": "document.getElementById(\"p\").innerHTML = \"Hello World!\";",
+                    "correct": false
+                }
+            ]
+        },
+        {
+            "question": "Where is the correct place to insert a JavaScript",
+            "answers": [
+                {
+                    "value": "The <main> section",
+                    "correct": false
+                },
+                {
+                    "value": "The <body> section",
+                    "correct": true
+                },
+                {
+                    "value": "The <head> section",
+                    "correct": false
+                },
+                {
+                    "value": "The <footer> section",
+                    "correct": false
+                }
+            ]
+        }
+    ]
 };
 
 // Element selectors
+const jumbotron = document.getElementById("jumbotron");
 const mainContent = document.getElementById("content");
+const secondsStart = 90;
+let secondsLeft = secondsStart;
+let questionItem = 0;
+let timer;
 
 // ---------------- Functions ----------------
 function init() {
@@ -75,15 +124,6 @@ function welcomeScreen() {
     showScoresEl.textContent = "View High Scores";
     userActions.appendChild(showScoresEl);
 
-
-    // REMOVE BEFORE SUBMITTING, TEMP FOR TESTING
-    let addScoresEl = document.createElement("button");
-    addScoresEl.setAttribute("id", "addScore");
-    addScoresEl.setAttribute("class", "btn");
-    addScoresEl.textContent = "Add Score";
-    userActions.appendChild(addScoresEl);
-    // ------------------------------------------
-
     welcomeDIV.appendChild(userActions);
 
     // Append to web page
@@ -93,6 +133,15 @@ function welcomeScreen() {
 function startQuiz() {
     // Clear the main content DIV
         mainContent.innerHTML = "";
+
+    // Reset userScore
+    localStorage.setItem("codeKnowledgeQuizUserScore", 0);
+
+    // Reset Timer
+    secondsLeft = secondsStart;
+
+    // Reset quiz
+    questionItem = 0;
 
     // Create game board for quiz
     let quizDIV = document.createElement("div");
@@ -110,7 +159,7 @@ function startQuiz() {
         optionHotKeyCount = i+1
         optionEl.setAttribute("id", "option" + optionHotKeyCount);
 
-        let hotkeyEl = document.createElement("div");
+        let hotkeyEl = document.createElement("span");
         hotkeyEl.setAttribute("class", "hotkey");
         hotkeyEl.textContent = i+1;
         optionEl.appendChild(hotkeyEl);
@@ -125,6 +174,21 @@ function startQuiz() {
     // Append to web page
     mainContent.appendChild(quizDIV);
 
+    // Build quiz questions number for the quiz
+    let quizNumDIV = document.createElement("div");
+    quizNumDIV.innerHTML = `Question:&nbsp;<span id=\"quizNumQuestions\">n/a</span>&nbsp;of ${quizQuestions.questions.length}`;
+    jumbotron.appendChild(quizNumDIV);
+
+    // Build score for the quiz
+    let quizScoreDIV = document.createElement("div");
+    quizScoreDIV.innerHTML = "Score:&nbsp;<span id=\"quizScore\">n/a</span>&nbsp;";
+    jumbotron.appendChild(quizScoreDIV);
+
+    // Build timer for the quiz
+    let quizTimerDIV = document.createElement("div");
+    quizTimerDIV.innerHTML = "Time:&nbsp;<span id=\"quizTimer\">00</span>";
+    jumbotron.appendChild(quizTimerDIV);
+
     document.getElementById("questionnaire").scrollIntoView();
     runQuiz()
 }
@@ -137,17 +201,125 @@ function runQuiz() {
     const optionThreeDIV = document.getElementById("option3");
     const optionFourDIV = document.getElementById("option4");
 
-    questionDIV.textContent = quizQuestions.item.question;
-    optionOneDIV.children[1].append(quizQuestions.item.answers[0].value);
-    optionTwoDIV.children[1].append(quizQuestions.item.answers[1].value);
-    optionThreeDIV.children[1].append(quizQuestions.item.answers[2].value);
-    optionFourDIV.children[1].append(quizQuestions.item.answers[3].value);
+    questionDIV.textContent = quizQuestions.questions[questionItem].question;
+    optionOneDIV.children[1].textContent = quizQuestions.questions[questionItem].answers[0].value;
+    optionTwoDIV.children[1].textContent = quizQuestions.questions[questionItem].answers[1].value;
+    optionThreeDIV.children[1].textContent = quizQuestions.questions[questionItem].answers[2].value;
+    optionFourDIV.children[1].textContent = quizQuestions.questions[questionItem].answers[3].value;
+
+    // Update the Quiz Question Number
+    updateQuizNum(questionItem + 1);
+
+    // Update the User Score
+    updateQuizScore();
+
+    updateQuizTimer();
+}
+
+// Update Quiz Number
+function updateQuizNum(number) {
+    // Update the quiz number
+    const quizNumQuestions = document.getElementById("quizNumQuestions");
+
+    quizNumQuestions.textContent = number
+}
+
+// Update Quiz Score
+function updateQuizScore() {
+    // Update the user score
+    const quizScore = document.getElementById("quizScore");
+
+    let userScore = localStorage.getItem("codeKnowledgeQuizUserScore");
+    if (userScore === null) {
+        userScore = 0;
+    }
+
+    quizScore.textContent = userScore;
+}
+
+// Update the Quiz Timer
+function updateQuizTimer() {
+    const quizTimer = document.getElementById("quizTimer");
+
+    timer = setInterval( function() {
+        secondsLeft--;
+
+        if(secondsLeft === 0) {
+            //Stop execution of action at set interval
+            clearInterval(timer);
+
+            // Move to Add Score page.
+            addScore();
+        }
+
+        localStorage.setItem("codeKnowledgeQuizTimer", secondsLeft);
+        
+        quizTimer.textContent = secondsLeft;
+
+    }, 1000);
+}
+
+// Check the user input against the answer key
+function checkAnswer(input) {
+    console.log("User selected answer #" + input);
+
+    if(input === "1" && quizQuestions.questions[questionItem].answers[0].correct) {
+        localStorage.setItem("codeKnowledgeQuizUserScore", parseInt(localStorage.getItem("codeKnowledgeQuizUserScore")) + 10);
+        secondsLeft = secondsLeft + 10
+    } else if(input === "2" && quizQuestions.questions[questionItem].answers[1].correct) {
+        localStorage.setItem("codeKnowledgeQuizUserScore", parseInt(localStorage.getItem("codeKnowledgeQuizUserScore")) + 10);
+        secondsLeft = secondsLeft + 10
+    } else if(input === "3" && quizQuestions.questions[questionItem].answers[2].correct) {
+        localStorage.setItem("codeKnowledgeQuizUserScore", parseInt(localStorage.getItem("codeKnowledgeQuizUserScore")) + 10);
+        secondsLeft = secondsLeft + 10
+    } else if(input === "4" && quizQuestions.questions[questionItem].answers[3].correct) {
+        localStorage.setItem("codeKnowledgeQuizUserScore", parseInt(localStorage.getItem("codeKnowledgeQuizUserScore")) + 10);
+        secondsLeft = secondsLeft + 10
+    } else {
+        secondsLeft = secondsLeft - 10
+    }
+
+    questionItem++
+
+    // Update the Quiz Question Number
+    updateQuizNum(questionItem + 1);
+
+    // Update the User Score
+    updateQuizScore();
+
+    iterateQuestion();
+}
+
+// Move to the next question
+function iterateQuestion() {
+
+    if(questionItem < quizQuestions.questions.length) {
+        const questionDIV = document.getElementById("question");
+        const optionOneDIV = document.getElementById("option1");
+        const optionTwoDIV = document.getElementById("option2");
+        const optionThreeDIV = document.getElementById("option3");
+        const optionFourDIV = document.getElementById("option4");
+    
+        questionDIV.textContent = quizQuestions.questions[questionItem].question;
+        optionOneDIV.children[1].textContent = quizQuestions.questions[questionItem].answers[0].value;
+        optionTwoDIV.children[1].textContent = quizQuestions.questions[questionItem].answers[1].value;
+        optionThreeDIV.children[1].textContent = quizQuestions.questions[questionItem].answers[2].value;
+        optionFourDIV.children[1].textContent = quizQuestions.questions[questionItem].answers[3].value;
+    } else {
+        addScore();
+    }
+    console.log(document.activeElement);
+    document.activeElement.blur();
 }
 
 // Build add score page.
 function addScore() {
     // Clear the main content DIV
-        mainContent.innerHTML = "";
+    mainContent.innerHTML = "";
+    jumbotron.innerHTML = "";
+
+    // Clear the timer
+    clearInterval(timer);
 
     // Create the div container for message
     let addScoreDIV = document.createElement("div");
@@ -168,7 +340,7 @@ function addScore() {
 
     let overviewElThree = document.createElement("span");
     overviewElThree.setAttribute("id", "userScore");
-    overviewElThree.textContent = "40";
+    overviewElThree.textContent = localStorage.getItem("codeKnowledgeQuizUserScore");
     overviewElTwo.appendChild(overviewElThree);
     addScoreDIV.appendChild(overviewElTwo);
 
@@ -298,8 +470,9 @@ init();
 // ------------- Event Listeners -------------
 // Add event listener to buttons
 mainContent.addEventListener("click", function(event) {
-    if (event.target.matches("button") === true) {
-        switch (event.target.id) {
+    if (event.target.matches("button") === true || event.target.parentNode.matches("button") === true) {
+        const id = event.target.id || event.target.parentNode.id;
+        switch (id) {
             case "welcomeScreen":
                 welcomeScreen();
                 break;
@@ -310,7 +483,7 @@ mainContent.addEventListener("click", function(event) {
             case "option2":
             case "option3":
             case "option4":
-                console.log(event.target.id);
+                checkAnswer(event.target.id.slice(-1));
                 break;
             case "addScore":
                 addScore();
@@ -330,6 +503,16 @@ mainContent.addEventListener("click", function(event) {
 })
 
 // Add event listener to quiz hotkeys
-// mainContent.addEventListener("keydown", function(event) {
-//     console.log(event.code);
-// })
+document.addEventListener("keyup", function(event) {
+    let optionsKeys = "1234".split("");
+
+    if (secondsLeft === 0) {
+        return;
+    }
+
+    if (optionsKeys.includes(event.key)) {
+        checkAnswer(event.key.toString());
+
+    }
+
+})
